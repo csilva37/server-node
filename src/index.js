1afb15c1dddfe13da476bb61ca27595e24d3e6b5
csilva37 <csilva37@stu.southtexascollege.edu>
2022-03-24 14:35:19 -0500

    Added Logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import config from'./utils/config'
 import express from 'express'
+import logger from './utils/logger'
 
 const app = express()
+
+app.use(logger.middleware)
 
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
+    logger.log.success('Calling Root')
     res.send({msg: 'Hello There!' })
 })
 
